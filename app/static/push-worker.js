08e647e5a1ab6292124152eb3584b9ee6847fecf
vasily.v <vasily.v@lineir.me>
2023-02-01 18:45:11 +0200

    Revert "GRAVITEC TEST"
    This reverts commit d5afeabc7b5313b1d4e6d8d75e999b206890e6f0.

--- app/static/push-worker.js
+++
@@ -1,6 +0,0 @@
-self[`appKey`] = `e9a63f7d0c58a3160560257192d2ac65`;
-self[`hostUrl`] = `https://cdn.gravitec.net/sw`;
-self.importScripts(`${self[`hostUrl`]}/worker.js`);
-// uncomment and set path to your service worker
-// if you have one with precaching functionality (has oninstall, onactivate event listeners)
-// self.importScripts('path-to-your-sw-with-precaching')
